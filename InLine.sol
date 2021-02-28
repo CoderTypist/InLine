decfd986966f3c769ab80de835bc7e5ed4494da4
Christian Bargraser <39784989+CoderTypist@users.noreply.github.com>
2021-02-28 17:29:22 -0600

    Update InLine.sol
    No need to pass msg.value as a parameter to called functions.

--- InLine.sol
+++ InLine.sol
@@ -2,26 +2,14 @@ pragma solidity 0.5.0;
 
 contract InLine {
     
-    // TODO: Make sure the contract works as expected so far
-    // TODO: Allow the contract owner to adjust weiPerMonth
-    //       - public function to check weiPerMonth
-    //       - in addFunds, user can specify expected max weiPerMonth
-    //         in case weiPerMonth suddenly changes
-    // TODO: Limit how far ahead a person can pay for subscription
+    // 5700 blocks per day * 30 days per month = 171,000
+    uint constant blocksPerMonth = 171000;
     
-    // monthly subscription cost (roughly $5)
-    // At the time of writing the contract, $5 is roughly 3,400,000,000,000,000 wei
-    // 1 finney is 1,000,000,000,000,000 wei
-    // 3,400,000,000,000,000 wei can be expressed as 3.4 finney
-    uint weiPerMonth = 100;
+    // monthly subscription cost 
+    uint constant weiPerMonth = 3400000000000000;
     
-    // Once a user subscribes, their status can never be NOTEXIST.
-    // Without NOTEXIST, myStatus() and getStatus() would return 0 for
-    // a non-existent user and a SINGLE user.
-    // { 0: NOTEXIST, 1: SINGLE, 2: COMPLICATED, 3: DATING, 4: MARRIED }
     enum Status {
         
-        NOTEXIST,
         SINGLE, 
         COMPLICATED,
         DATING, 
@@ -30,10 +18,10 @@ contract InLine {
     
     struct User {
         
+        uint blockJoined;
+        uint blockExpiration;
         Status status;
-        uint timeJoined;
-        uint timeExpiration;
-        uint timeLastChange;
+        uint blockLastChange;
     }
     
     // Sent whenever a User changes their relationship status
@@ -55,69 +43,39 @@ contract InLine {
     mapping(address => User) subs;
     
     modifier userExists {
-        require(subs[msg.sender].timeJoined != 0);
+        require(subs[msg.sender].blockJoined != 0);
         _;
     }
     
-    modifier userPaid {
-        require(subs[msg.sender].timeExpiration >= now);
+    modifier userPaid() {
+        require(subs[msg.sender].blockExpiration <= block.number);
         _;
     }
     
     modifier validStatus(Status _status) {
-        require(_status == Status.SINGLE
-            || _status == Status.COMPLICATED 
-            || _status == Status.DATING 
-            || _status == Status.MARRIED);
-        _;
-    }
-    
-    modifier otherExists(address _userAddr) {
-        require(subs[_userAddr].timeJoined != 0);
+        require(_status == Status.SINGLE || _status == Status.COMPLICATED || _status == Status.DATING || _status == Status.MARRIED);
         _;
     }
     
     // testing (remove when finished)
-    function contractBalance()
-        external
-        view
-        returns (uint)
-    {
+    function balance() external view returns (uint256) {
         return address(this).balance;
     }
     
     // Unscribed user subscribes, sets status, and adds initial funds
-    function subscribe(uint _months, Status _status)
-        external
-        payable
-        validStatus(_status)
-        returns (bool)
-    {
-        
-        // Make sure that the user does not already exist
-        require(subs[msg.sender].timeJoined == 0);
+    function subscribe(uint _months, Status _status) external payable validStatus(_status) {
         
         if (true == processFunds(_months, msg.value)) { 
             
             User storage user = subs[msg.sender];
-            user.timeJoined = now;
+            user.blockJoined = block.number;
             user.status = _status;
-            user.timeLastChange = now;
-            emit Change(msg.sender, Status.NOTEXIST, _status);
-            
-            return true;
+            user.blockLastChange = block.number;
         }
-        
-        // Did not subscribe
-        return false;
     }
     
     // Subscribed user adds additional funds
-    function addFunds(uint _months)
-        external
-        payable
-        userExists
-    {
+    function addFunds(uint _months) external payable userExists {
         processFunds(_months, msg.value);
     }
     
@@ -127,34 +85,33 @@ contract InLine {
         require(_months > 0);
         
         uint monthsPossible = _wei / weiPerMonth;
-        uint numWeeks = _months*4;
         
         // if enough wei was provided to pay for the specified _months
         if (monthsPossible >= _months) {
             
             User storage user = subs[msg.sender];
             
-            // if the user has not previously subscribed - invoked by subscribe()
-            if (0 == user.timeJoined) {
-                user.timeExpiration = now + (numWeeks * 1 weeks);
+            // if the user has not previously subscribed
+            if (0 == user.blockJoined) {
+                user.blockExpiration = block.number + (_months * blocksPerMonth);
             }
             
-            // if the user has previously subscribed - invoked by addFunds()
+            // if the user has previously subscribed
             else {
                 
                 // if the users subscription has expired
-                if (user.timeExpiration < now) {
-                    user.timeExpiration = now + (numWeeks * 1 weeks);
+                if (user.blockExpiration < block.number) {
+                    user.blockExpiration = block.number + (_months * blocksPerMonth);
                 }
                 
                 // if the users subscription has not expired
                 else {
-                    user.timeExpiration += numWeeks;
+                    user.blockExpiration += _months * blocksPerMonth;
                 }
             }
             
             // send change back
-            uint change = _wei - (_months * weiPerMonth);
+            uint change = msg.value - (_months * weiPerMonth);
             msg.sender.transfer(change);
             
             return true;
@@ -162,9 +119,21 @@ contract InLine {
         
         // if not enough wei was provided, return the ether
         else {
-            msg.sender.transfer(_wei);
+            msg.sender.transfer(msg.value);
             return false;
         }
+    }
+
+    function myStatus() external view userExists userPaid returns (Status) {
+        
+        return subs[msg.sender].status;
+    }
+
+    function getStatus(address _userAddr) external view userExists userPaid returns (Status) {
+        
+        // Other address must have subscribed at some point
+        require(subs[_userAddr].blockJoined != 0);
+        return subs[_userAddr].status;
     }
     
     function statusChange(Status _status) internal {
@@ -175,39 +144,13 @@ contract InLine {
         User storage user = subs[msg.sender];
         Status before = user.status;
         user.status = _status;
-        user.timeLastChange = now;
+        user.blockLastChange = block.number;
         
         emit Change(msg.sender, before, _status);
     }
     
-    function statusProof(address _recipient) 
-        external
-        userExists
-        userPaid
-    {
+    function statusProof(address _recipient) external userExists userPaid {
+        
         emit Proof(msg.sender, _recipient, subs[msg.sender].status);
-    }
-    
-    function myUser()
-        external
-        view
-        userExists
-        userPaid
-        returns(Status, uint, uint, uint)
-    {
-        User storage user = subs[msg.sender];
-        return (user.status, user.timeJoined, user.timeExpiration, user.timeLastChange);
-    }
-    
-    function getUser(address _userAddr)
-        external
-        view
-        userExists
-        userPaid
-        otherExists(_userAddr)
-        returns(Status, uint, uint, uint)
-    {
-        User storage user = subs[_userAddr];
-        return (user.status, user.timeJoined, user.timeExpiration, user.timeLastChange);
     }
 }
