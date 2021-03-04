0f282982877339377ff08ba472331b861cdfff7a
Christian Bargraser <39784989+CoderTypist@users.noreply.github.com>
2021-03-03 22:11:02 -0600

    Update InLine.sol
    subscribe() and addFunds() now require the user to specify how months they want to subscribe for. Created _verifyPayment modifier.

--- InLine.sol
+++ InLine.sol
@@ -1,8 +1,5 @@
 pragma solidity 0.5.0;
 
-// Error: USER CAN SUBSCRIBE FOR FREE!!! (Fixed by changing a >= to >)
-// This means that they can update their status for free. We want them to pay. 
-
 contract InLine {
     
     // TODO: Make sure the contract works as expected so far
@@ -23,6 +20,7 @@ contract InLine {
     // ONLY SET TO 100 FOR TESTING
     // ONLY SET TO 100 FOR TESTING
     uint weiPerMonth = 100;
+    address owner;
     
     // Once a user subscribes, their status can never be NOTEXIST.
     // Without NOTEXIST, myStatus() and getStatus() would return 0 for
@@ -86,111 +84,119 @@ contract InLine {
         _;
     }
     
-    // testing (remove when finished)
-    function contractBalance()
-        external
-        view
-        returns (uint)
-    {
-        return address(this).balance;
+    /*
+     * Why is it necessary to specify _months when months can be calculated from the value?
+     * Let's say that the price per month is 100 wei and that a person wants to sign up for 4 months.
+     * The user creates a transaction signing them up for 4 months and sends 400 wei.
+     * Let's say than the owner of the contract then creates a transaction adjusing the price per month to 50 wei.
+     * If the owner's transaction is included in the block first, the price per month
+     * will change before the user's transaction is processed.
+     * Since the user sent 400 wei, they would be forced into an 8 month subscription that they might not have wanted.
+     * By specifying how many months they wanted, the user will be refunded any extra wei. 
+     * 
+     * Why is it necessary to specify _maxWeiPerMonth?
+     * Let's say that the price per month is 100 wei and that a person wants to sign up for 4 months
+     * Given that the price per month can change, a person could send extra wei to account for a sudden change in price
+     * Any extra wei will be returned to the sender
+     */
+    modifier verifyPayment(uint _months, uint _maxWeiPerMonth) {
+        
+        // Make sure the user is willing to pay the current monthly fee
+        // This is to avoid the contract owner unexpectedly changing the monthly fee
+        require(_maxWeiPerMonth <= weiPerMonth);
+        
+        // Make sure that enough ether was sent
+        require((msg.value/weiPerMonth)>=_months);
+        _;
+    }
+    
+    modifier isOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor() public {
+        owner = msg.sender;
     }
     
     // Unscribed user subscribes, sets status, and adds initial funds
-    function subscribe(Status _status)
+    function subscribe(Status _status, uint _months, uint _maxWeiPerMonth)
         external
         payable
         validStatus(_status)
-        returns (bool)
+        verifyPayment(_months, _maxWeiPerMonth)
     {
-        
         // Make sure that the user does not already exist
         require(subs[msg.sender].timeJoined == 0);
         
-        if (true == processFunds()) { 
-            
-            User storage user = subs[msg.sender];
-            user.timeJoined = now;
-            user.status = _status;
-            user.timeLastChange = now;
-            emit Change(msg.sender, Status.NOTEXIST, _status);
-            
-            return true;
-        }
+        // Creates an account for the user
+        processFunds(_months);
         
-        // Did not subscribe
-        return false;
+        User storage user = subs[msg.sender];
+        user.timeJoined = now;
+        user.status = _status;
+        user.timeLastChange = now;
+        emit Change(msg.sender, Status.NOTEXIST, _status);
     }
     
     // Subscribed user adds additional funds
-    function addFunds()
+    function addFunds(uint _months, uint _maxWeiPerMonth)
         external
         payable
         userExists
+        verifyPayment(_months, _maxWeiPerMonth)
     {
-        processFunds();
+        // Make sure the user is willing to pay the current monthly fee
+        // This is to avoid the contract owner unexpectedly changing the monthly fee
+        require(_maxWeiPerMonth <= weiPerMonth);
+        
+        processFunds(_months);
     }
     
     // Called by subscribe() and addFunds() for updating user.timeExpiration
-    function processFunds() internal returns (bool) {
+    function processFunds(uint _months) internal {
         
-        uint monthsPossible = msg.value / weiPerMonth;
-        uint numWeeks = monthsPossible*4;
+        uint numWeeks = _months*4;
+        User storage user = subs[msg.sender];
         
-        // if enough wei was provided to pay for the specified _months
-        if (monthsPossible > 0) {
+        // if the user has not previously subscribed - invoked by subscribe()
+        if (0 == user.timeJoined) {
             
-            User storage user = subs[msg.sender];
+            // ONLY FOR TESTING - REMOVE LATER
+            // ONLY FOR TESTING - REMOVE LATER
+            // ONLY FOR TESTING - REMOVE LATER
+            user.timeExpiration = now + (numWeeks * 1 seconds);
             
-            // if the user has not previously subscribed - invoked by subscribe()
-            if (0 == user.timeJoined) {
+            // user.timeExpiration = now + (numWeeks * 1 weeks);
+        }
+        
+        // if the user has previously subscribed - invoked by addFunds()
+        else {
+            
+            // if the users subscription has expired
+            if (user.timeExpiration < now) {
                 
                 // ONLY FOR TESTING - REMOVE LATER
                 // ONLY FOR TESTING - REMOVE LATER
                 // ONLY FOR TESTING - REMOVE LATER
-                user.timeExpiration = now + (monthsPossible * 1 seconds);
+                user.timeExpiration += (numWeeks * 1 seconds);
+                
                 // user.timeExpiration = now + (numWeeks * 1 weeks);
             }
             
-            // if the user has previously subscribed - invoked by addFunds()
+            // if the users subscription has not expired
             else {
                 
-                // if the users subscription has expired
-                if (user.timeExpiration < now) {
-                    
-                    // ONLY FOR TESTING - REMOVE LATER
-                    // ONLY FOR TESTING - REMOVE LATER
-                    // ONLY FOR TESTING - REMOVE LATER
-                    user.timeExpiration += (monthsPossible * 1 seconds);
-                    
-                    // user.timeExpiration = now + (numWeeks * 1 weeks);
-                }
+                // ONLY FOR TESTING - REMOVE LATER
+                // ONLY FOR TESTING - REMOVE LATER
+                // ONLY FOR TESTING - REMOVE LATER
+                user.timeExpiration += (numWeeks * 1 seconds);
                 
-                // if the users subscription has not expired
-                else {
-                    
-                    // ONLY FOR TESTING - REMOVE LATER
-                    // ONLY FOR TESTING - REMOVE LATER
-                    // ONLY FOR TESTING - REMOVE LATER
-                    user.timeExpiration += (monthsPossible * 1 seconds);
-                    
-                    //user.timeExpiration += (numWeeks * 1 weeks);
-                }
+                //user.timeExpiration += (numWeeks * 1 weeks);
             }
-            
-            // send change back
-            uint change = msg.value - (monthsPossible * weiPerMonth);
-            msg.sender.transfer(change);
-            
-            return true;
-        }
-        
-        // if not enough wei was provided, return the ether
-        else {
-            msg.sender.transfer(msg.value);
-            return false;
         }
     }
-    
+
     function statusChange(Status _status) internal {
         
         // Status should change
@@ -239,10 +245,33 @@ contract InLine {
         return (user.status, user.timeJoined, user.timeExpiration, user.timeLastChange);
     }
     
-    // REMOVE LATER, ONLY FOR TESTING
-    // REMOVE LATER, ONLY FOR TESTING
-    // REMOVE LATER, ONLY FOR TESTING
+    function setWeiPerMonth(uint _weiPerMonth) external isOwner {
+        weiPerMonth = _weiPerMonth;
+    }
+    
+    // FOR TESTING, REMOVE LATER
+    // #####################################################################################
+    
+    // testing (remove when finished)
+    function contractBalance()
+        external
+        view
+        returns (uint)
+    {
+        return address(this).balance;
+    }
+    
     function myBalance() public view returns(uint) {
         return msg.sender.balance;
     }
+    
+    function getOwner() public view returns(address) {
+        return owner;
+    }
+    
+    function getWeiPerMonth() public view returns(uint) {
+        return weiPerMonth;
+    }
+    
+    // #####################################################################################
 }
